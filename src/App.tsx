ed836ccdb395a75392fe5739d1ce136c484f3257
bblanc <tranlongh2003@gmail.com>
2025-03-11 15:02:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Admin, Resource, ShowContext, ShowGuesser } from "react-admin";
+import { Layout } from "./Layout";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
+import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
+
+
+
+export const App = () => (
+  <Admin authProvider={authProvider} layout={Layout} dataProvider={dataProvider} dashboard={Dashboard} >
+      <Resource 
+        name="posts" 
+        list={PostList} 
+        edit={PostEdit} 
+        create={PostCreate}
+        icon={PostIcon}
+      />
+      <Resource
+        name="users" 
+        list={UserList} 
+        show={ShowGuesser} 
+        icon={UserIcon}
+      />
+  </Admin>
+);
---
+++ src/posts.tsx
@@ -0,0 +1,52 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+} from "react-admin";
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" />,
+];
+
+export const PostList = () => {
+    return (
+        <List filters={postFilters}>
+            <Datagrid>
+                <ReferenceField source="userId" reference="users" link="show" />
+                <TextField source="id" />
+                <TextField source="title" />
+                <TextField source="body" />
+            </Datagrid>
+        </List>
+    );
+};
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" />
+            <ReferenceInput source="userId" reference="users"  link="show" />
+            
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Create>
+);
