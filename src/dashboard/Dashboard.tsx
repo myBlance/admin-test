749ca9acbbab5bf2496ba1d32350a12a8486f398
bblanc <tranlongh2003@gmail.com>
2025-03-31 09:26:10 +0700

    cap nhat code

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -15,7 +15,14 @@ export const Dashboard = () => (
             <CardHeader title="Welcome to the administration" />
             
             <CardContent>
-                <Box sx={{ height: 600, width:800, ml:-10 }} >
+                <Box sx={{ 
+                    height: 400, 
+                    width: 800, 
+                    ml: 5, 
+                    border: "2px solid #000", 
+                    borderRadius: 3, 
+                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)" 
+                }}>
                     <PieChartWithTinyLineChart />
                 </Box>
             </CardContent>
